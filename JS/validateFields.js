0aaa418bf67c4edd8e3050fdd4b8af4a934edad0
Thiago <thiagosilva.2020@alunos.utfpr.edu.br>
2022-04-27 15:36:26 -0300

    Thiago - INPUT

--- JS/validateFields.js
+++ JS/validateFields.js
@@ -1,7 +1,14 @@
 buttonClick = function () {
     let login = document.getElementById('telaLogin');
     let busca = document.getElementById('telaBusca');
-
+    let inputEmail = document.getElementById('inputEmail');
+    let inputPassword = document.getElementById('inputPassword');
+    if (!inputEmail.value || !inputPassword.value) {
+        return;
+    }
+    if(inputEmail.value.lenght < 3 || inputPassword.value.lenght < 3) {
+        return;
+    }
     login.style.display = 'none';
     busca.style.display = 'block';
 }
