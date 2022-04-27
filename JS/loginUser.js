a8d71f421f6ddb019cbd762c7c728b10b6fd7ed5
vanessaaugutis <vanessa.augutis@clinicarx.com.br>
2022-04-26 22:01:00 -0300

    Vanessa - requisicao de login feita

--- JS/loginUser.js
+++ JS/loginUser.js
@@ -1,8 +1,54 @@
+//cria o método de http para conectar com api
+let xmlhttp = new XMLHttpRequest();
+
+//Abre a api desejada
+xmlhttp.open(
+	'GET', 'https://reqres.in/api/users', true
+);
+
+xmlhttp.onreadystatechange = function () {
+    if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+        let retorno = JSON.parse(xmlhttp.responseText);
+        console.log(retorno.data);
+    }
+};
+
+xmlhttp.send();
+
 openLogin = function() {
     let login = document.getElementById('telaLogin');
     let discord = document.getElementById('telaDiscord');
 
     login.style.display = 'flex';
     discord.style.display = 'none';
+}
+
+buttonClick = function () {
+
+    xmlhttp.open(
+        'POST', 'https://reqres.in/api/login', true
+    );
+
+    let email = document.getElementById("emailUser");
+    let password = document.getElementById("passwordUser");
+    let userValid = false;
+
+    xmlhttp.onreadystatechange = function () {
+        if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 400) {
+            alert("E-mail ou senha incorretos");
+        } else if (xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+            userValid = true;
+            let login = document.getElementById('telaLogin');
+            let busca = document.getElementById('telaBusca');
+        
+            if (userValid) {
+                login.style.display = 'none';
+                busca.style.display = 'block';
+            }
+        }
+    };
+
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({ "email": email.value, "password": password.value }));
 }
 
