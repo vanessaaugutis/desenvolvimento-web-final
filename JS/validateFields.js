af74f56d35daad24efdc3f806f5820208cb6600f
Thiago <thiagosilva.2020@alunos.utfpr.edu.br>
2022-05-06 12:12:31 -0300

    Thiago - API

--- JS/validateFields.js
+++ JS/validateFields.js
@@ -19,3 +19,37 @@ buttonClick = function () {
     validateLogin();
 }
 
+searchItem = function () {
+    let inputSearch = document.getElementById('search-text');
+    let url = 'https://www.cheapshark.com/api/1.0/games?id=' + inputSearch.value;
+
+    xmlhttp.open(
+        'GET', url, true
+    );
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 400) {
+            alert("Erro ao buscar");
+        } else
+            if (xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+                let retorno = JSON.parse(xmlhttp.responseText);
+                if (!retorno.info) {
+                    alert("ID INVÁLIDO");
+                    getListUnits();
+                } else {
+                    let telaList = document.getElementById('list-units');
+                    while (telaList.firstChild) {
+                        telaList.removeChild(telaList.firstChild);
+                    }
+
+                    telaList.appendChild(createMenuItem(inputSearch.value, retorno.info.title));
+                }
+            }
+    };
+
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xmlhttp.send();
+}
+
+
