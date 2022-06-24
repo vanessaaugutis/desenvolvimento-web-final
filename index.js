cb5e3333be8fc7eb76faccad6040708c489d3951
unknown <vanessa.augutis@clinicarx.com.br>
2022-06-24 20:02:00 -0300

    Vanessa - configuração heroku

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(5000, () => console.log(`Listening on ${ 5000 }`));
+  .listen(process.env.PORT || 5000, () => console.log(`Listening on ${ process.env.PORT || 5000 }`));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
